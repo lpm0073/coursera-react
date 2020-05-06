e9f516013581a11c811e953eec5ef30b0064451f
Lawrence McDaniel <lpm0073@gmail.com>
2020-05-05 20:09:42 -0500

    Homework 1 Task 3

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -11,26 +11,77 @@ class DishDetail extends Component {
         this.setState({ dish: props.dish});
     }
 
+    renderDish(dish) {
+        console.log("Menu DishDetail renderDish invoked")
+        if (dish != null)
+            return(
+                <div  className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width="100%" top src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+);
+        else
+            return(
+                <div></div>
+            );
+    }
+
+    renderComments(comments) {
+        console.log("Menu DishDetail renderComments invoked")
+
+        if (comments != null) {
+            const list_items = this.props.dish.comments.map((comment) => {
+                return (
+                  <li key={comment.id} className="">
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author}, {comment.date}</p>
+                  </li>
+                );
+            });
+
+            return (
+                <div  className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardBody>
+                            <CardTitle><h4>Comments</h4></CardTitle>
+                            <CardText>
+                                <ul class = "list-unstyled">
+                                    {list_items}
+                                </ul>
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+    
+            )
+    
+        }
+        else 
+            return(
+                <div></div>
+            )
+    }
+
     render() {
         console.log("Menu DishDetail render is invoked")
         const dish = this.props.dish;
 
-        if (dish != null)
+        if (dish != null) {
+    
             return(
                 <div className="container">
                     <div className="row">
-                        <div  className="col-12 m-1">
-                            <Card>
-                                <CardImg width="100%" top src={dish.image} alt={dish.name} />
-                                <CardBody>
-                                    <CardTitle>{dish.name}</CardTitle>
-                                    <CardText>{dish.description}</CardText>
-                                </CardBody>
-                            </Card>
-                        </div>
+                        {this.renderDish(dish)}
+                        {this.renderComments(dish.comments)}
                     </div>
                 </div>
             );
+        }
         else
             return(
                 <div></div>
@@ -38,6 +89,5 @@ class DishDetail extends Component {
 
     }
 }
-
 
 export default DishDetail;
