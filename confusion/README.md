766337103dd9915dd069265671b9d040df6dfbcb
Lawrence McDaniel <lpm0073@gmail.com>
2020-05-07 07:20:54 -0500

    Homework 2 Task 1

--- confusion/README.md
+++ confusion/README.md
@@ -1,5 +1,9 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+
+https://www.coursera.org/learn/front-end-react/peer/ptyN7/react-router-and-single-page-applications
+
+
 ## Available Scripts
 
 In the project directory, you can run:
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -36,6 +37,16 @@ class Main extends Component {
       );
     }
     
+    const AboutPage = () => {
+      return(
+        <About 
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]} 
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leaders={this.state.leaders}
+          />
+      );
+    }
+
     const DishWithId = ({match}) => {
       return(
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} 
@@ -52,6 +63,7 @@ class Main extends Component {
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={AboutPage} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
