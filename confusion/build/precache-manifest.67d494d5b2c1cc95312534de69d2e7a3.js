fb584450e662b1d325dc60b60f15362e8d3e6b6f
Lawrence McDaniel <lpm0073@gmail.com>
2020-05-10 18:06:53 -0500

    publish to github pages

---
+++ confusion/build/precache-manifest.67d494d5b2c1cc95312534de69d2e7a3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b090aa4ff7dbd093f6557199017f6f4e",
+    "url": "/coursera-react/index.html"
+  },
+  {
+    "revision": "64eb3d6374ec401ec226",
+    "url": "/coursera-react/static/css/2.6df3aa63.chunk.css"
+  },
+  {
+    "revision": "956f78dae6f58c7cdfa7",
+    "url": "/coursera-react/static/css/main.5e828a71.chunk.css"
+  },
+  {
+    "revision": "64eb3d6374ec401ec226",
+    "url": "/coursera-react/static/js/2.1c16ddb4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/coursera-react/static/js/2.1c16ddb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "956f78dae6f58c7cdfa7",
+    "url": "/coursera-react/static/js/main.64cd80f7.chunk.js"
+  },
+  {
+    "revision": "a9d2cbcaa6df1407e006",
+    "url": "/coursera-react/static/js/runtime-main.9640085a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/coursera-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/coursera-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/coursera-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/coursera-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/coursera-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
